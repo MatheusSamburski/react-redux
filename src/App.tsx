6579ee2aa9b1022b2ae398f88d968c31932dc48a
MatheusSamburski <mateus41789@gmail.com>
2023-08-28 15:46:35 -0300

    projeto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
+import { Player } from "./pages/Player";
+import "./styles/global.css"
+
+export function App() {
+  return (
+    <div>
+      <ReduxProvider store={store}>
+        <Player />
+      </ReduxProvider>
+    </div>
+  );
+}
